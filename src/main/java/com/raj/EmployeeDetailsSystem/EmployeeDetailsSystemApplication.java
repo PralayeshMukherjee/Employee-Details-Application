127eae2f63025eda3d3b08619cfe16d40003c09a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-07 13:47:24 +0530

    Initilize the Spring Boot application with MySQL,JPA,Web dependency

---
+++ src/main/java/com/raj/EmployeeDetailsSystem/EmployeeDetailsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.raj.EmployeeDetailsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDetailsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDetailsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raj/EmployeeDetailsSystem/EmployeeDetailsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.EmployeeDetailsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDetailsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
